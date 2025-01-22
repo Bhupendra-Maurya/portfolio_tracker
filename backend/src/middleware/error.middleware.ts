1adfdc7803610c16e2ab6a4189c6afe91cbe0745
Bhupendra <bpmaurya24th@gmail.com>
2025-01-22 16:16:15 +0530

    add global error handler

--- backend/src/middleware/error.middleware.ts
+++ backend/src/middleware/error.middleware.ts
@@ -1,11 +1,16 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { HttpError } from "http-errors";
 
-export const globalErrorHandler = (err: HttpError, req: Request, res: Response) => {
+export const globalErrorHandler = (
+  err: HttpError,
+  req: Request,
+  res: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  next: NextFunction
+) => {
   const statusCode = err.statusCode || 500;
   res.status(statusCode).json({
-    message: err.message,
-    stack: process.env.NODE_ENV === "development" ? err.stack : "",
+    message: err.message || "Internal Server Error",
+    stack: process.env.NODE_ENV === "development" ? err.stack : undefined,
   });
-  return;
 };
